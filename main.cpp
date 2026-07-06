b82ba8d514c133c6a1009652b335f010af09db0c
John-Gallardo <johnpatrickgallardo33@gmail.com>
2026-07-05 18:00:41 -0600

    Feat: created CMakeLists.txt & main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// Vulkan
+#define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
+#include <vulkan/vulkan_raii.hpp>
+
+// Windowing Library
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+int main() {
+    return 0;
+}
