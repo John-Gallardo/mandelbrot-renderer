5dd912524f9b55d7a9c43c2b3e696f1e0233d85d
John-Gallardo <johnpatrickgallardo33@gmail.com>
2026-07-08 15:17:38 -0600

    refactor: align by initializer instead of name

--- src/Config.h
+++ src/Config.h
@@ -3,7 +3,7 @@
 #include <string_view>
 
 namespace Config{
-    inline constexpr uint32_t         windowWidth {800};
-    inline constexpr uint32_t         windowHeight{600};
-    inline constexpr std::string_view appTitle    {"Mandelbrot Renderer"};
+    inline constexpr uint32_t windowWidth     {800};
+    inline constexpr uint32_t windowHeight    {600};
+    inline constexpr std::string_view appTitle{"Mandelbrot Renderer"};
 }
