7302d4655d452ac13a3e321e044a9f94d7ee880f
John-Gallardo <johnpatrickgallardo33@gmail.com>
2026-08-06 18:48:17 -0600

    refactor: align value of maxFramesInFlight

--- src/Config.h
+++ src/Config.h
@@ -6,5 +6,5 @@ namespace Config{
     inline constexpr uint32_t windowWidth     {800};
     inline constexpr uint32_t windowHeight    {600};
     inline constexpr std::string_view appTitle{"Mandelbrot Renderer"};
-    inline constexpr int maxFramesInFlight{2};
+    inline constexpr int maxFramesInFlight    {2};
 }
