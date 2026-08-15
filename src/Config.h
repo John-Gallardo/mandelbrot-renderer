6a3363afe6f6f68cd21f67b1a5a1f318d1e1e303
John-Gallardo <johnpatrickgallardo33@gmail.com>
2026-08-14 21:57:59 -0600

    feat: add moveConstant & zoomRate to config

--- src/Config.h
+++ src/Config.h
@@ -7,4 +7,6 @@ namespace Config{
     inline constexpr uint32_t windowHeight    {1000};
     inline constexpr std::string_view appTitle{"Mandelbrot Renderer"};
     inline constexpr int maxFramesInFlight    {2};
+    inline constexpr double moveConstant      {0.5};
+    inline constexpr double zoomRate          {2};
 }
